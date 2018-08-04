7886d85e8a497242780d26614a5865c630b6607e
Cortez21 <mailto:cortezzz1987@gmail.com>
2018-08-04 10:43:16 +0300

    Refactored Fight's class

--- src/main/java/Fighter.java
+++ src/main/java/Fighter.java
@@ -1,139 +1,61 @@
+import java.util.Random;
+
 /**
  * @author cortezzz (mailto:cortezzz1987@gmail.com)
  * @version 1.0
  * Описывает боевую единицу в игре
  */
-public class Fighter {
-    /*
-    name - имя данного персонажа
-    nameOfEnemy - имя противника
-    ifNpc - в качестве параметра значение 1 или 2 (1 - ИИ, 2 - живой игрок)
-    specialization - класс бойца. значение от 1 до 4 (1 - Thief, 2 - Asassin, 3 - Warrior, 4 - Paladin)
-    block - позиция текущей блокировки аттаки от 1 до 3 (1 - голова, 2 - торс, 3 - ноги)
-    Примечание: У класса Paladin блокировка торса идет по умолчанию всегда. Это исключение реализовано в методе combat.
-     */
-    final private String name;
-    final private boolean ifNpc;
-    final private int specialization;
-    private int currentHealth = 100;
-    private int block = 1;
+public abstract class Fighter {
+
+    private final String name;
+    private int health = 100;
     private int target;
-    private int currentDamage;
-    private boolean breakUp = false;
-    private boolean dodge = false;
-    private boolean crit = false;
+    private int block;
+    private int damage;
 
-    public Fighter(String name, boolean ifNpc, int specialization) {
+
+    public Fighter(String name) {
         this.name = name;
-        this.ifNpc = ifNpc;
-        this.specialization = specialization;
     }
 
-    public String getName() {
-        return this.name;
+    public void setBlock(int block) {
+        this.block = block;
     }
 
-    public int getSpecialization() {
-        return specialization;
+    public void setTarget(int target) {
+        this.target = target;
     }
 
-    public int getCurrentHealth() {
-        return currentHealth;
+    private void setDamage(int damage) {
+        this.damage = damage;
     }
 
-    public int getBlock() {
-        return block;
-    }
-
-    public int getTarget() {
-        return target;
-    }
-
-    public int getCurrentDamage() {
-        return currentDamage;
-    }
-
-    public boolean isCrit() {
-        return crit;
-    }
-
-    public boolean isBreakUp() {
-        return breakUp;
-    }
-
-    public boolean isDodge() {
-        return dodge;
-    }
-
-    public void combat(int enemyDamage, int target, boolean crit, boolean breakUp, Fighter enemy) {
-
-        //Здесь описан алгоритм блокировки удара. Если класс данного персонажа Paladin (блокипуються точки 1 или 3)
-        //В остальных случаях блокируется одна из 3х точек
-        double blockMod = Math.random();
-        if (this.specialization == 4 && blockMod < 0.5) {
-            this.block = 1;
-        } else if (this.specialization == 4 && blockMod >= 0.5) {
-            this.block = 3;
-        } else if (blockMod < 0.33) {
-            this.block = 1;
-        } else if (blockMod >= 0.33 && blockMod < 0.66) {
-            this.block = 2;
-        } else {
-            this.block = 3;
+    public void turn(int target, int damage) {
+        if (!blocking(target)) {
+            drainHealth(damage);
         }
 
-        //Здесь высчитываеться вероятность уклонения (шанс - 25%) если класс персонажа thief
-        double dodgeMod = Math.random();
-        if (this.specialization == 1 && dodgeMod < 0.25) {
-            this.dodge = true;
-            System.out.println("dodgeMod is: " + dodgeMod);
-        }
+        generateDamage(4, 10);
+    }
 
-        //Проверяются условия блокировки удара, уклонения, либо срабатывания умения breakUp класса warrior
-        //Если атака противника все же прошла, от текущего здоровья персонажа отнимаеться урон противника
-        if ((target != this.block && !this.dodge) || (target == this.block && breakUp && !this.dodge)) {
-            this.currentHealth -= enemyDamage;
+    public boolean blocking(int target) {
+        if (block == target) {
+            return true;
         }
+        return false;
+    }
 
-        //расчет повреждений персонажа в текущем раунде
-        double damageMod = Math.random();
-        if (damageMod < 0.2) {
-            this.currentDamage = 6;
-        } else if (damageMod >= 0.2 && damageMod < 0.4) {
-            this.currentDamage = 7;
-        } else if (damageMod >= 0.4 && damageMod < 0.6) {
-            this.currentDamage = 8;
-        } else if (damageMod >= 0.6 && damageMod < 0.8) {
-            this.currentDamage = 9;
-        } else {
-            this.currentDamage = 10;
-        }
+    public void drainHealth(int damage) {
+        health -= damage;
+    }
 
-        //Расчет вероятности критического удара (шанс - 25%). Если класс персонажа Asassin и крит прошел - урон увеличиваеться вдвое.
-        if (this.specialization == 2 && 0.25 < Math.random()) {
-            this.currentDamage *= 2;
-            this.crit = true;
-        }
+    public void generateDamage(int min, int max) {
+        setDamage(numberGenerator(min, max));
+    }
 
-        //Инициализация позиции нападения
-        double targetMod = Math.random();
-        if (enemy.getSpecialization() == 4 && targetMod < 0.5) {
-            this.target = 1;
-        } else if (enemy.getSpecialization() == 4 && targetMod >= 0.5) {
-            this.target = 3;
-        } else if (targetMod < 0.33) {
-            this.target = 1;
-        } else if (targetMod >= 0.33 && targetMod < 0.66) {
-            this.target = 2;
-        } else {
-            this.target = 3;
-        }
-
-        // Если класс персонажа warrior - срабатывает умение breakUp
-        if (this.specialization == 3) {
-            this.breakUp = true;
-        }
-
+    public int numberGenerator(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(max - min + 1) + min;
     }
 
 }
--- src/main/java/TestMode.java
+++ src/main/java/TestMode.java
@@ -1,27 +1,17 @@
+import java.util.Random;
+
 public class TestMode {
 
     public static void main(String[] args) {
-        Fighter fighter1 = new Fighter("Galatar", true, 1);
-        Fighter fighter2 = new Fighter("CortezZz", true, 4);
-        int cycl = 100;
-
-        int countOfDodge = 0;
-        for (int i = 0; i < cycl; i++) {
-            fighter1.combat(8, 3, false, false, fighter2);
-            if (fighter1.isDodge()) {
-                countOfDodge++;
-            }
-            System.out.println(fighter1.getName());
-            System.out.println("Health: " + fighter1.getCurrentHealth() + "(8)");
-            System.out.println("Block: " + fighter1.getBlock() + "(3)");
-            System.out.println("Damage: " + fighter1.getCurrentDamage());
-            System.out.println("Target: " + fighter1.getTarget());
-            System.out.println("Dodge: " + fighter1.isDodge());
-            System.out.println("Crit: " + fighter1.isCrit());
-            System.out.println("BreakUp: " + fighter1.isBreakUp());
-            System.out.println("Random is: " + Math.random());
-            System.out.println("******************************************************");
+        TestMode test = new TestMode();
+        for (int i = 0; i < 50; i++){
+            test.generateDamage(3, 10);
         }
-        System.out.println("Count dodge: " + countOfDodge + " of " + cycl);
     }
+
+    public void generateDamage(int min, int max) {
+        Random random = new Random();
+        System.out.println(random.nextInt(max - min + 1) + min);
+    }
+
 }
